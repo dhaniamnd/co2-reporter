d5f36de0c302b93822497759fc3c57349ff84f20
Dhani Amanda Putra <dhaniamanda04@gmail.com>
2025-08-13 01:51:35 +0700

    Update ResultsTable.tsx
    border update

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -12,19 +12,19 @@ export default function ResultsTable({ rows, totals }: { rows: OutputRow[]; tota
             ))}
           </tr>
         </thead>
-        <tbody>
+        <tbody className=''>
           {rows.map((r,i) => (
             <tr key={i} className="odd:bg-[var(--brand-102)] even:bg-[var(--brand-103)]">
-              <td className="px-3 py-2">{r.Plant}</td>
-              <td className="px-3 py-2">{new Date(r.Date).toLocaleDateString()}</td>
-              <td className="px-3 py-2">{r.Month}</td>
-              <td className="px-3 py-2 text-right">{r.input.Clinker_t.toLocaleString()}</td>
-              <td className="px-3 py-2 text-right">{r.input.KilnFuel_GJ.toLocaleString()}</td>
-              <td className="px-3 py-2 text-right">{r.input.Electricity_MWh.toLocaleString()}</td>
-              <td className="px-3 py-2 text-right">{r.Process_tCO2.toFixed(2)}</td>
-              <td className="px-3 py-2 text-right">{r.Fuel_tCO2.toFixed(2)}</td>
-              <td className="px-3 py-2 text-right">{r.Electric_tCO2.toFixed(2)}</td>
-              <td className="px-3 py-2 text-right font-semibold">{r.Total_tCO2.toFixed(2)}</td>
+              <td className="border px-3 py-2">{r.Plant}</td>
+              <td className="border px-3 py-2">{new Date(r.Date).toLocaleDateString()}</td>
+              <td className="border px-3 py-2">{r.Month}</td>
+              <td className="border px-3 py-2 text-right">{r.input.Clinker_t.toLocaleString()}</td>
+              <td className="border px-3 py-2 text-right">{r.input.KilnFuel_GJ.toLocaleString()}</td>
+              <td className="border px-3 py-2 text-right">{r.input.Electricity_MWh.toLocaleString()}</td>
+              <td className="border px-3 py-2 text-right">{r.Process_tCO2.toFixed(2)}</td>
+              <td className="border px-3 py-2 text-right">{r.Fuel_tCO2.toFixed(2)}</td>
+              <td className="border px-3 py-2 text-right">{r.Electric_tCO2.toFixed(2)}</td>
+              <td className="border px-3 py-2 text-right font-semibold">{r.Total_tCO2.toFixed(2)}</td>
             </tr>
           ))}
         </tbody>
